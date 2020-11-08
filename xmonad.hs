29ab81210a545851bd7d75600bef47ac8d89eb91
james willett <jms.wlltt@gmail.com>
2020-11-07 23:45:22 -0500

    better full layout, recompilation message

--- xmonad.hs
+++ xmonad.hs
@@ -142,7 +142,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
 
     -- Restart xmonad
-    , ((modm              , xK_q     ), spawn "~/.xmonad/recompile.sh; xmonad --restart")
+    , ((modm              , xK_q     ), spawn "~/.xmonad/recompile.sh; xmonad --restart; echo 'recompiled!' | dzen2 -p 1")
 
     -- Run xmessage with a summary of the default keybindings (useful for beginners)
     , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
@@ -202,25 +202,25 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- which denotes layout choice.
 --
 myLayout =
-  avoidStruts
-  $ smartBorders
-  $ spacingRaw True sb True sb True
-  $ tiled ||| Mirror tiled ||| Full
+  smartBorders $ (_avoidStruts tiled) ||| Full
     where
-       -- default tiling algorithm partitions the screen into two panes
-       tiled   = Tall nmaster delta ratio
+      -- default tiling algorithm partitions the screen into two panes
+      tiled   = Tall nmaster delta ratio
 
-       -- The default number of windows in the master pane
-       nmaster = 1
+      -- The default number of windows in the master pane
+      nmaster = 1
 
-       -- Default proportion of screen occupied by master pane
-       ratio   = 1/2
+      -- Default proportion of screen occupied by master pane
+      ratio   = 1/2
 
-       -- Percent of screen to increment by when resizing panes
-       delta   = 3/100
+      -- Percent of screen to increment by when resizing panes
+      delta   = 3/100
 
-       px      = 10
-       sb      = Border { top = px, bottom = px, left = px, right = px }
+      px      = 10
+      sb      = Border { top = px, bottom = px, left = px, right = px }
+
+      _spacingRaw  = spacingRaw True sb True sb True
+      _avoidStruts = avoidStruts . _spacingRaw
 
 ------------------------------------------------------------------------
 -- Window rules:
