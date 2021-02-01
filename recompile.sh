c138bb82c96c7577c935308983925ac9b1e48262
James Willett <jms.wlltt@gmail.com>
2021-02-01 11:31:36 -0500

    arch hates linked packages, btw

--- recompile.sh
+++ recompile.sh
@@ -1,1 +1,1 @@
-`ghc --make ~/.xmonad/xmonad.hs -package xmonad -i -ilib -dynamic -fforce-recomp -main-is main -v0 -o ~/.xmonad/xmonad-x86_64-linux; xmonad --restart ; echo 'recompiled!' | dzen2 -p 1;`
+`ghc --make ~/.xmonad/xmonad.hs -package xmonad -package xmonad-contrib -i -ilib -dynamic -fforce-recomp -main-is main -v0 -o ~/.xmonad/xmonad-x86_64-linux; xmonad --restart ; echo 'recompiled!' | dzen2 -p 1;`
