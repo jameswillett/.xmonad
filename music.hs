744a24531c152ef90278283c685ae499cfe09814
James Willett <jms.wlltt@gmail.com>
2023-04-21 23:30:15 -0400

    better get player function and colors

--- music.hs
+++ music.hs
@@ -19,12 +19,14 @@ wrap pre suff string = pre ++ string ++ suff
 wrapAtt :: String -> String
 wrapAtt = wrap "{{" "}}"
 
+wrapColor hex = wrap ("<fc=#" ++ hex ++ ">") "</fc>"
+
 getPlayerIcon player =
   case player of
     -- Gets unicode char for nerd font
-    "chromium" -> "\62056"
-    "spotify"  -> "\61884"
-    "vlc"      -> "\984444"
+    "chromium" -> wrapColor "DB4437" "\62056"
+    "spotify"  -> wrapColor "1DB954" "\61884"
+    "vlc"      -> wrapColor "E47B00" "\984444"
     _            -> ""
 
 getStatusIcon status =
@@ -33,33 +35,40 @@ getStatusIcon status =
     "Paused"  -> "⏸️"
     _         -> ""
 
-stripLeadingDash (' ':'-':' ':xs) = xs
-stripLeadingDash x                = x
-
 isError :: ExitCode -> Bool
 isError ExitSuccess = False
 isError _           = True
 
-getPlayer        = trim <$> readProcess "playerctl" ["metadata", "-f", "{{playerName}}"] ""
+choose a b
+  | null a    = b
+  | otherwise = a
+
+getPlayer statuses  = do
+  players <- lines <$> readProcess "playerctl" ["metadata", "-f", "{{playerName}}", "-a"] ""
+  let playersWithStatuses = zip (filter (/= "Stopped") statuses) players
+  let pausedPlayers = map snd $ filter ((=="Paused") . fst) playersWithStatuses
+  let playingPlayers = map snd $ filter ((=="Playing") . fst) playersWithStatuses
+  return $ head $ choose playingPlayers pausedPlayers
+
 getStatus player = trim <$> readProcess "playerctl" ["status", "-p", player] ""
 getAttribute player att = trim <$> readProcess "playerctl" ["metadata", "-f", wrapAtt att, "-p", player] ""
 
 main = do
   (code, statuses, err) <- readProcessWithExitCode "playerctl" ["status", "-a"] ""
   if isError code || all (== "Stopped") (lines statuses) then return () else do
-    player            <- getPlayer
+    player            <- getPlayer (lines statuses)
     status            <- trim <$> readProcess "playerctl" ["status", "-p", player] ""
     let _getAttribute =  getAttribute player
     artist            <- _getAttribute "artist"
     title             <- _getAttribute "title"
     currTime          <- _getAttribute "duration(position)"
     totTime           <- _getAttribute "duration(mpris:length)"
-    putStrLn $ unwords 
-      $ filter (/= mempty) 
+    putStrLn $ unwords
+      $ filter (/= mempty)
       [ getPlayerIcon player
       , getStatusIcon status
-      , if artist /= mempty then artist ++ " -" else ""
-      , wrap "<fc=#ccc>" "</fc>" $ clampLength 100 title
+      , if artist /= mempty then wrapColor "aaa" artist ++ " -" else ""
+      , wrapColor "ccc" $ clampLength 100 title
       , currTime
       , wrap "(" ")" totTime
       ]
